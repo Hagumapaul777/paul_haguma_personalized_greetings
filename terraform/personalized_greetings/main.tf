d83a44344f813e5b6678163e3cafdcbeaa9246f8
Paul H <hagumapaul.777@gmail.com>
2024-12-07 10:49:55 +0200

    vpc module creation

--- terraform/personalized_greetings/main.tf
+++ terraform/personalized_greetings/main.tf
@@ -14,8 +14,17 @@ module "ec2_instance" {
   key_name      = "13th_july_2024_personalized_greetings"                # Replace with your SSH key name
   instance_name = "Assignment_machine2"
   region        = "us-east-1"                 # AWS region (optional)
+  subnet_id = element(module.vpc.public_subnet_ids, 0)
 }
 
-output "instance_public_ip" {
-  value = module.ec2_instance.instance_public_ip
+module "vpc" {
+  source               = "../modules/vpc"
+  cidr_block           = "10.0.0.0/16"
+  vpc_name             = "Personalized-Greetings-VPC"
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnet_cidrs = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  default_tags = {
+    Environment = "Dev"
+    Project     = "Personalized-Greetings"
   }
+}
